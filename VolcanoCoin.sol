24a18e9200d3b4a2eb7366b7fc3bf2cec324236d
ericbadiere <eric.badiere@coinme.com>
2022-10-27 15:50:45 -0600

    Fixes from code review.

--- VolcanoCoin.sol
+++ VolcanoCoin.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: None
+/ SPDX-License-Identifier: None
 
 pragma solidity 0.8.0;
 
@@ -14,22 +14,17 @@ contract VolcanoCoin is Ownable{
     );
 
     event Sent(
+        address sender,
         uint256 amount,
         string message,
         address recipient
     );
 
-    struct UserBalance{
-        address user;
-        uint256 balance;
-    }
-
     struct Payment{
+        address sender;
         address recipientAddress;
         uint256 transferAmount;
     }
-    
-    Payment[] userPayments;
 
     mapping (address => uint256) balances;
     mapping (address => Payment[]) payments;
@@ -47,32 +42,29 @@ contract VolcanoCoin is Ownable{
     }
 
     function increaseTotalSupply() public onlyOwner {
-        totalSupply = totalSupply + 1000;
+        totalSupply += 1000;
         balances[owner()] = totalSupply;
         emit Increase(1000, "added to total supply");
-    
     }
 
     function transfer(uint256 amount, address to) public payable {
         require (amount <= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender] - amount;
+        balances[to] += amount;
+        
         recordPayment(msg.sender, to, amount);
+
+        emit Sent(msg.sender, amount, "sent to", to);
+
     }
-    
+
+    function recordPayment(address sender, address recipient, uint256 amount) public payable {
+        payments[sender].push(Payment({sender: sender, recipientAddress: recipient, transferAmount: amount})); 
+    }
+
     function getPayments(address user) public view returns (Payment[] memory){
         return payments[user];
     }
-
-    function recordPayment(address sender, address receiver, uint256 amount) public payable {
-        require (amount <= balances[sender]);
-        balances[sender] = balances[sender] - amount;
-        balances[receiver] += amount;
-        
-        userPayments.push(Payment({recipientAddress: receiver, transferAmount: amount}));
-        
-        payments[sender] = userPayments;
-
-        emit Sent(amount, "sent to", sender);
-
-
-    }
+    
 }
+
